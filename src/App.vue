2de01abae7b8c95cb01de5626bc343b8fa2004f6
Oludare.Theophilus <Oludare.Theophilus@anyskillsinc.onmicrosoft.com>
2023-09-19 23:36:58 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>
+      Hackathon
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa adipisci est ipsa soluta reiciendis. Quibusdam nemo pariatur et, sint sunt nostrum assumenda sapiente quis incidunt amet ratione possimus delectus accusantium suscipit velit earum mollitia eos corrupti perspiciatis expedita. Perspiciatis fuga est nihil blanditiis architecto quas rem quo? Deserunt consequuntur fuga dicta ratione iusto aspernatur fugiat. Iure deserunt ad nostrum architecto illo magni quam eum culpa ut laborum reprehenderit, maxime fuga deleniti amet, dolor sunt veniam ratione earum neque id fugiat itaque vitae praesentium. Debitis tempore, illo voluptas, nemo, aliquam fugit quia alias culpa laudantium unde sit cum voluptatum eius quis!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
